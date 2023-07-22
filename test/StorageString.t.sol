cec0ca1f8e0c4884f8163f595e3d330dfb4f3083
cds-amal <cds.sudama@gmail.com>
2023-07-21 23:20:29 -0400

    fix get/set

--- test/StorageString.t.sol
+++ test/StorageString.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import {HuffConfig} from "foundry-huff/HuffConfig.sol";
 import {HuffDeployer} from "foundry-huff/HuffDeployer.sol";
 import {NonMatchingSelectorHelper} from "./test-utils/NonMatchingSelectorHelper.sol";
@@ -18,14 +19,18 @@ contract StorageStringTest is Test, NonMatchingSelectorHelper {
         str = StorageString(HuffDeployer.config().deploy("StorageString"));
     }
 
-    function testSumArray() external {
-        string memory text = "hello world";
+    function test_fuzz_strings(string calldata text) external {
+        vm.assume(bytes(text).length > 0);
+
         str.setValue(text);
         string memory stored = str.getValue();
-        assertEq(stored, text, "expected to store hello world");
+        console.log("stored: %s", stored);
+        console.log("stored: %s", text);
+        assertEq(stored, text, text);
     }
 
     /// @notice Test that a non-matching selector reverts
+    /// forge-config: default.fuzz.runs = 0
     function testNonMatchingSelector(bytes32 callData) public {
         bytes4[] memory func_selectors = new bytes4[](1);
         func_selectors[0] = StorageString.getValue.selector;
